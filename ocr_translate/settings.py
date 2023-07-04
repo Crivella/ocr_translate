67abe8e93fa044e20317fbbf4906a8310fec1fbe
Crivella <davidecrivella@yahoo.it>
2023-07-04 23:39:46 +0200

    Fix for big images

--- ocr_translate/settings.py
+++ ocr_translate/settings.py
@@ -122,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10*1024*1024
