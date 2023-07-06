a14166de62adb90cfc51b737dc839422a8adf56e
Crivella <davidecrivella@yahoo.it>
2023-07-07 00:46:32 +0200

    Added admin interface to manually add models

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from . import models as m
+
 # Register your models here.
+
+class OCRBoxModelAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class OCRModelAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class TSLModelAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(m.OCRBoxModel, OCRBoxModelAdmin)
+admin.site.register(m.OCRModel, OCRModelAdmin)
+admin.site.register(m.TSLModel, TSLModelAdmin)
