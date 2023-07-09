118bc4c8b054a210fa1d404762e18d3c4b1656c7
Crivella <davidecrivella@yahoo.it>
2023-07-09 15:33:40 +0200

    Added on-start autocreate of languages and validated models

--- base/OCR_TSL/base.py
+++ base/OCR_TSL/base.py
@@ -1,9 +1,12 @@
 # import json
+import json
 import os
 from pathlib import Path
 
 from transformers import (AutoImageProcessor, AutoModel, AutoModelForSeq2SeqLM,
                           AutoTokenizer, VisionEncoderDecoderModel)
+
+from .. import models as m
 
 # from .. import models as m
 
@@ -52,15 +55,34 @@ def load_model(model_id: str, request: list[str]):
 
     return res
 
+if os.environ.get('AUTOCREATE_LANGUAGES', 'false').lower() == 'true':
+    cwd = Path(__file__).parent
+    with open(cwd / 'languages.json') as f:
+        langs = json.load(f)
 
+    for lang in langs:
+        m.Language.objects.get_or_create(**lang)
 
-# def import_models():
-#     with open(root / "config.json") as f:
-#         config = json.load(f)
+if os.environ.get('AUTOCREATE_VALIDATED_MODELS', 'false').lower() == 'true':
+    cwd = Path(__file__).parent
+    with open(cwd / 'models.json') as f:
+        models = json.load(f)
 
-#     for name in config["ocr_models"]:
-#         m.OCRModel.objects.get_or_create(name=name)
-#     for name in config["tsl_models"]:
-#         m.TSLModel.objects.get_or_create(name=name)
+    for box in models['box']:
+        model, _ = m.OCRBoxModel.objects.get_or_create(**box)
 
-    
+    for ocr in models['ocr']:
+        lang = ocr.pop('lang')
+        model, _ = m.OCRModel.objects.get_or_create(**ocr)
+        for l in lang:
+            model.languages.add(m.Language.objects.get(iso1=l))
+
+    for tsl in models['tsl']:
+        src = tsl.pop('lang_src')
+        dst = tsl.pop('lang_dst')
+        model, _ = m.TSLModel.objects.get_or_create(**tsl)
+        for l in src:
+            model.src_languages.add(m.Language.objects.get(iso1=l))
+
+        for l in dst:
+            model.dst_languages.add(m.Language.objects.get(iso1=l))
