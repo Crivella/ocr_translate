1d1c914f6e4fd96e2eadb3b52ad53bcad899a137
Crivella <davidecrivella@yahoo.it>
2023-07-26 18:24:37 +0200

    Test coverage for tesseract

--- ocr_translate/ocr_tsl/tesseract.py
+++ ocr_translate/ocr_tsl/tesseract.py
@@ -48,7 +48,7 @@ def download_model(lang: str):
         ValueError: If the model could not be downloaded.
     """
     if not DOWNLOAD:
-        raise ValueError('Downloading models is not allowed')
+        raise ValueError('TESSERACT_ALLOW_DOWNLOAD is false. Downloading models is not allowed')
     create_config()
 
     logger.info(f'Downloading tesseract model for language {lang}')
