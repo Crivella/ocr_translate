e8e718382f5720857eacf953d3e30ce9e3a82db6
Crivella <davidecrivella@yahoo.it>
2023-09-17 07:18:00 +0200

    Fix needed to carry entrypoints with pyinstaller

--- build.sh
+++ build.sh
@@ -8,12 +8,14 @@ pyinstaller \
     --icon icon.ico \
     --add-data "ocr_translate/ocr_tsl/languages.json:ocr_translate/ocr_tsl" \
     --add-data "ocr_translate/ocr_tsl/models.json:ocr_translate/ocr_tsl" \
+    --collect-all djang-ocr_translate \
     --collect-all torch \
     --collect-all torchvision \
     --collect-all transformers \
     --collect-all unidic_lite \
     --collect-all sacremoses \
     --collect-all sentencepiece \
+    --recursive-copy-metadata djang-ocr_translate \
     --recursive-copy-metadata torch \
     --recursive-copy-metadata torchvision \
     --recursive-copy-metadata transformers \
