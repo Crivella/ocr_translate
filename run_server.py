7e2fa639945d1047ee7e23b0c9e4f06331e1b003
Crivella <davidecrivella@yahoo.it>
2023-10-29 16:12:56 +0100

    Improved run_server.py

--- run_server.py
+++ run_server.py
@@ -17,7 +17,7 @@
 # Home: https://github.com/Crivella/ocr_translate                                 #
 ###################################################################################
 """Run the django migrations and start the server."""
-# pylint: disable=import-outside-toplevel
+# pylint: disable=import-outside-toplevel,too-many-statements,invalid-name
 
 import importlib
 import os
@@ -26,18 +26,27 @@ from pathlib import Path
 import django
 from django.core.management import call_command
 
+from ocr_translate import __version__
+
 
 def main():
     """Run the django migrations and start the server."""
+    print('--------------------------------------------')
+    print('-                                          -')
+    print(f'-     OCR_TRANSLATE version v{__version__:<13s} -')
+    print('-                                          -')
+    print('--------------------------------------------')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     os.environ.setdefault('DJANGO_DEBUG', 'True')
     os.environ.setdefault('DJANGO_LOG_LEVEL', 'INFO')
 
     home = Path.home()  / '.ocr_translate'
     if not 'TRANSFORMERS_CACHE' in os.environ:
+        print(f'TRANSFORMERS_CACHE not set:  Using "{home}" as transformers cache')
         os.environ['TRANSFORMERS_CACHE'] = str(home)
         home.mkdir(exist_ok=True, parents=True)
     if not 'DATABASE_NAME' in os.environ:
+        print(f'DATABASE_NAME nto set:  Using "{home / "db.sqlite3"}" as database')
         os.environ['DATABASE_NAME'] = str(home / 'db.sqlite3')
         home.mkdir(exist_ok=True, parents=True)
 
@@ -51,21 +60,42 @@ def main():
             print('CUDA is not available, falling back to using CPU')
             os.environ['DEVICE'] = 'cpu'
         else:
+            print('CUDA is available, using GPU')
             os.environ.setdefault('DEVICE', 'cuda')
 
+    print('Running django setup...')
     django.setup()
 
     from ocr_translate.ocr_tsl.initializers import (auto_create_languages,
                                                     auto_create_models,
                                                     init_most_used)
 
+    print('Create database if needed and apply database migrations (if any)...')
     call_command('migrate')
 
-    auto_create_languages()
-    auto_create_models()
+    su_name = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+    su_pass = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'password')
+    if su_name and su_pass:
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        if not User.objects.filter(username=su_name).exists():
+            print(f'Creating superuser "{su_name}" with password "{su_pass}"')
+            User.objects.create_superuser(su_name, '', su_pass)
+
+
+    ac_lang = os.environ.get('AUTO_CREATE_LANGUAGES', 'true')
+    if ac_lang == 'true':
+        print('Autocreate language entries in database...')
+        auto_create_languages()
+    ac_models = os.environ.get('AUTOCREATE_VALIDATED_MODELS', 'true')
+    if ac_models == 'true':
+        print('Autocreate validated models in database...')
+        auto_create_models()
     if os.getenv('LOAD_ON_START', 'false') == 'true':
+        print('Load most used models in memory...')
         init_most_used()
 
+    print('Starting server...')
     call_command('runserver', '--noreload', '4000')
 
 if __name__ == '__main__':
