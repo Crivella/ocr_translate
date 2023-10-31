ae70eef3bf170fe34e6ade0b49fea1aef517ba1f
Crivella <davidecrivella@yahoo.it>
2023-10-31 03:30:17 +0100

    Added autocorrect capabiliy to Trie

--- ocr_translate/trie.py
+++ ocr_translate/trie.py
@@ -28,6 +28,9 @@ class TrieNode:
     def __str__(self) -> str:
         return f'TrieNode(childs: {list(self.children.keys())}, is_word: {self.is_word})'
 
+    def __contains__(self, key: str) -> bool:
+        return key in self.children
+
     def __getitem__(self, key: str) -> 'TrieNode':
         return self.children.get(key, None)
 
@@ -52,7 +55,7 @@ class Trie:
         node.is_word = True
         node.freq = freq
 
-    def search(self, word: str, strict: bool = False) -> bool:
+    def search(self, word: str, strict: bool = False, node = None) -> bool:
         """Search a word in the Trie.
 
         Args:
@@ -60,11 +63,13 @@ class Trie:
             strict (bool, optional): If True, return False if the word contains characters not in the Trie.
                 If False, ignore characters not in the Trie.
                 Defaults to False.
+            node (TrieNode, optional): Current node in the recursion. Defaults to None.
 
         Returns:
             bool: True if the word is in the Trie, False otherwise.
         """
-        node = self.root
+        if node is None:
+            node = self.root
 
         for char in word:
             if char not in self.charset:
@@ -152,7 +157,7 @@ class Trie:
 
         Args:
             word (str): Word to decompose.
-            min_length (int, optional): Minimum lengeth of the words to be decomposed into. Defaults to 3.
+            min_length (int, optional): Minimum length of the words to be decomposed into. Defaults to 3.
 
         Returns:
             list[list[str]]: list of all possible decompositions, where a decomposition is a list of words.
@@ -184,6 +189,177 @@ class Trie:
                     node.children[word[:i]] = self._decompose(word[i:], min_length)
 
         return res
+
+    def get_all_substitutions(self, word: str, max_num: int = 1) -> list[str]:
+        """Get all possible substitutions of a word.
+
+        Args:
+            word (str): Word to substitute.
+            max_num (int, optional): Maximum number of substitutions. Defaults to 2.
+
+        Returns:
+            list[str]: List of all possible substitutions.
+        """
+        if max_num > len(word):
+            max_num = len(word)
+
+        res = self._get_all_substitutions(word, max_num)
+        res = list(set(res))
+        if word in res:
+            res.remove(word)
+
+        res.sort(key=self.get_freq, reverse=True)
+
+        return res
+
+    def _get_all_substitutions(self, word: str, max_num: int = 1, invalid_ok: bool = False) -> list[str]:
+        """Recursive implementation of get_all_substitutions()."""
+        results = []
+
+        if max_num == 0:
+            return results
+
+        ptr = self.root
+        for i,char_in in enumerate(word):
+            for char, node in ptr.children.items():
+                if char == char_in:
+                    continue
+                new = word[:i] + char + word[i+1:]
+                if invalid_ok or self.search(word[i+1:], strict=True, node=node):
+                    results.append(new)
+                # Not in if: Possible 1 sub is not a valid word but 2 or more subs are
+                # appa -> apna -> anna (if appa and anna are words but apna is not)
+                results.extend(self._get_all_substitutions(new, max_num-1, invalid_ok=invalid_ok))
+            ptr  = ptr[char_in]
+            if ptr is None:
+                break
+
+        return results
+
+    def get_all_deletions(self, word: str, max_num: int = 1) -> list[str]:
+        """Get all possible deletions of a word.
+
+        Args:
+            word (str): Word to delete.
+            max_num (int, optional): Maximum number of deletions. Defaults to 2.
+
+        Returns:
+            list[str]: List of all possible deletions.
+        """
+        if max_num > len(word):
+            max_num = len(word)
+
+        res = self._get_all_deletions(word, max_num)
+        res = list(set(res))
+
+        res.sort(key=self.get_freq, reverse=True)
+
+        return res
+
+    def _get_all_deletions(self, word: str, max_num: int = 1) -> list[str]:
+        """Recursive implementation of get_all_deletions()."""
+        results = []
+
+        if max_num == 0:
+            return results
+
+        for i in range(len(word)):
+            new = word[:i] + word[i+1:]
+            if self.search(new, strict=True):
+                results.append(new)
+            results.extend(self._get_all_deletions(new, max_num-1))
+
+        return results
+
+    def get_all_insertions(self, word: str, max_num: int = 1) -> list[str]:
+        """Get all possible insertions of a word.
+
+        Args:
+            word (str): Word to insert.
+            max_num (int, optional): Maximum number of insertions. Defaults to 2.
+
+        Returns:
+            list[str]: List of all possible insertions.
+        """
+        res = self._get_all_insertions(word, max_num)
+        res = list(set(res))
+
+        res.sort(key=self.get_freq, reverse=True)
+
+        return res
+
+    def _get_all_insertions(self, word: str, max_num: int = 1) -> list[str]:
+        """Recursive implementation of get_all_insertions()."""
+        results = []
+
+        if max_num == 0:
+            return results
+
+        for i in range(len(word)+1):
+            for char in self.charset:
+                new = word[:i] + char + word[i:]
+                if self.search(new, strict=True):
+                    results.append(new)
+                results.extend(self._get_all_insertions(new, max_num-1))
+
+        return results
+
+    def autocorrect_bayes(self, word: str, max_num: int = 1, distance_exp_factor: float = 2) -> list[str]:
+        """Autocorrect a word bases on its probability.
+
+        Args:
+            word (str): Word to autocorrect.
+            max_num (int, optional): Maximum number of autocorrections. Defaults to 2.
+            distance_exp_factor (float, optional): Exponential (dist+1)^N factor for the distance of the autocorrection.
+
+        Returns:
+            list[str]: List of all possible autocorrections.
+        """
+        possible = []
+        if self.search(word):
+            possible.append((word, self.get_freq(word)))
+
+        possible.extend(self._autocorrect_bayes(word, max_num, distance_exp_factor))
+
+        possible.sort(key=lambda x: x[1], reverse=True)
+
+        res = []
+        for poss in possible:
+            if poss[0] not in res:
+                res.append(poss[0])
+
+        return res
+
+    def _autocorrect_bayes(self, word: str, max_num: int = 1, distance_exp_factor: float = 2) -> list[str]:
+        """Recursive implementation of autocorrect_bayes()."""
+        results = []
+
+        if max_num == 0:
+            return results
+
+        print('---------------')
+        print(word, max_num)
+        # print(self.root.children['a'])
+        # Also add all possible combinations of substitutions + deletion/insertion
+        for i in range(1, max_num):
+            for sub in  self._get_all_substitutions(word, i, invalid_ok=True):
+                print(f'{sub = }')
+                results.extend(self._get_all_deletions(sub, max_num-i))
+                results.extend(self._get_all_insertions(sub, max_num-i))
+
+        results.extend(self._get_all_substitutions(word, max_num))
+        print(results)
+        results.extend(self._get_all_deletions(word, max_num))
+        print(results)
+        results.extend(self._get_all_insertions(word, max_num))
+        print(results)
+
+        results = [(res, self.get_freq(res) / (max_num+1)**distance_exp_factor) for res in results]
+        print(results)
+
+        results.extend(self._autocorrect_bayes(word, max_num-1, distance_exp_factor))
+
+        return results
 
     def get_all_branches(self) -> list[list[str]]:
         """Return a list of all valid (terminating in a word) branches of the Trie.
