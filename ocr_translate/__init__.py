a410500cef300341ac0d12e951cd5647ec2769f4
Crivella <davidecrivella@yahoo.it>
2023-12-17 07:20:25 +0100

    Bumped version

--- ocr_translate/__init__.py
+++ ocr_translate/__init__.py
@@ -18,5 +18,5 @@
 ###################################################################################
 """OCR and translation of images."""
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __version__array__ = [int(_) for _ in __version__.split('.')]
