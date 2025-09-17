2a113426484d14486e977b1cf57dd18b58d4b5e5
crivella <davidecrivella@yahoo.it>
2025-09-17 12:18:43 +0200

    Allows to run properly with `tty` in docker to have colors

--- start-server.sh
+++ start-server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # start-server.sh
 
-echo "Create group and user with specified UID/GID"
+echo "Create group and user with specified UID=${UID} GID=${GID}"
 USER=runner
 if [ ! -z ${OCT_GUNICORN_USER} ]; then
     USER=${OCT_GUNICORN_USER}
@@ -20,8 +20,6 @@ chown -R ${USER}:${USER} /db_data
 echo "Starting nginx."
 nginx
 
-su ${USER}
-
 export TRANSFORMERS_CACHE=${TRANSFORMERS_CACHE:-"/models/huggingface"}
 export TRANSFORMERS_OFFLINE=${TRANSFORMERS_OFFLINE:-"0"}
 export EASYOCR_MODULE_PATH=${EASYOCR_MODULE_PATH:-"/models/easyocr"}
@@ -31,5 +29,7 @@ export PADDLEOCR_PREFIX=${PADDLEOCR_PREFIX:-"/models/paddleocr"}
 export OCT_DJANGO_PORT=4010
 export OCT_BASE_DIR="/plugin_data"
 
+export COLUMNS=${COLUMNS:-160}
+
 source /venv/bin/activate
-python run_server.py
+su ${USER} /bin/bash -c "python run_server.py"
